3352a4e64ec077e7669f41a74e01a1972fa4fc65
Leah Chen <leeewchen@gmail.com>
2022-06-13 11:23:29 -0700

    commit tree 0f5323d3c1f0ac39e145d1f92c37de543313cca9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
 
+    def initialize(discount=0)
+        @total= 0
+        @discount= discount
+        
+    end
+
+    def add_item(title, amount, quantity)
+
+
+    end
+
+
+   
+
+end
